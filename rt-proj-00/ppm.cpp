a7ad60170fd54f883e842de14eb5bd17e2f875cf
André Freitas <freitasandre38@gmail.com>
2024-03-08 18:17:02 -0300

    feat: create interpolate color

--- rt-proj-00/ppm.cpp
+++ rt-proj-00/ppm.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 #include "include/ppm.hpp"
 
 using namespace std;
@@ -12,6 +13,25 @@ void headerWriter(ofstream &file, int width, int height) {
 
 void pixelWriter(ofstream &file, int red, int green, int blue) {
     file << red << " " << green << " " << blue << "\n";
+}
+
+Color interpolateColor(const Color& A, const Color& B, const Color& C, const Color& D, double u, double v) {
+    // Interpolate between A and B with parameter u to get Xb
+    Color Xb = { round((1.0 - u) * A.red + u * B.red),
+                 round((1.0 - u) * A.green + u * B.green),
+                 round((1.0 - u) * A.blue + u * B.blue) };
+
+    // Interpolate between C and D with parameter u to get Xt
+    Color Xt = { round((1.0 - u) * C.red + u * D.red),
+                 round((1.0 - u) * C.green + u * D.green),
+                 round((1.0 - u) * C.blue + u * D.blue) };
+
+    // Interpolate between Xb and Xt with parameter v to get the final color I
+    Color I = { round((1.0 - v) * Xb.red + v * Xt.red),
+                round((1.0 - v) * Xb.green + v * Xt.green),
+                round((1.0 - v) * Xb.blue + v * Xt.blue) };
+
+    return I;
 }
 
 int main() {
