560a7ed4feeb8dc5d56cb26876e12d8411f3ce68
André Freitas <freitasandre38@gmail.com>
2024-03-08 18:29:35 -0300

    refactor: change functions

--- rt-proj-00/ppm.cpp
+++ rt-proj-00/ppm.cpp
@@ -17,7 +17,7 @@ void pixelWriter(ofstream &file, int red, int green, int blue) {
 }
 
 vector<double> interpolateColor(const vector<double>& A, const vector<double>& B,const vector<double>& C, const vector<double>& D, double u, double v) {
-    // P(t) = (1 - t)*A + t * B
+    // P(t) = (1 - t) * A + t * B
     
     vector<double> I_axb = {(1 - u) * A[0] + u * B[0],
                             (1 - u) * A[1] + u * B[1],
@@ -30,10 +30,6 @@ vector<double> interpolateColor(const vector<double>& A, const vector<double>& B
     vector<double> P = {(1 - v) * I_axb[0] + v * I_cxd[0],
                         (1 - v) * I_axb[1] + v * I_cxd[1],
                         (1 - v) * I_axb[2] + v * I_cxd[2]};
-
-    // for (auto& component : P) {
-    //     component = round(component);
-    // }
 
     return P;
 }
@@ -54,6 +50,8 @@ int main() {
     }
 
     headerWriter(ppmFile, width, height);
+    // User this for generate ppm as default part 1
+
     // for (int y = height; y > 0; y--) {
     //     for (int x = 0; x < width; x++) {
 
@@ -65,18 +63,15 @@ int main() {
     //     }
     // }
 
+    // Generation with bilinear interpolation
     for (int y = height; y > 0; y--) {
         for (int x = 0; x < width; x++) {
-            double u = static_cast<double>(x) / (width - 1);  // Normalize x to [0, 1]
-            double v = static_cast<double>(y - 1) / (height - 1);  // Normalize y to [0, 1]
+            double u = static_cast<double>(x) / (width - 1);
+            double v = static_cast<double>(y - 1) / (height - 1); 
 
-            // Perform bilinear interpolation to get the color
-            vector<double> interpolatedColor = interpolateColor(A, B, C, D, u, v);
+            vector<double> color = interpolateColor(A, B, C, D, u, v);
 
-            // Convert the interpolated color to integers and write to the file
-            pixelWriter(ppmFile, static_cast<int>(interpolatedColor[0]),
-                                        static_cast<int>(interpolatedColor[1]),
-                                        static_cast<int>(interpolatedColor[2]));
+            pixelWriter(ppmFile, static_cast<int>(color[0]), static_cast<int>(color[1]), static_cast<int>(color[2]));
         }
     }
 
