1a76ecb139089440fa22e2226794e55549cd4f34
André Freitas <freitasandre38@gmail.com>
2024-03-14 11:16:04 -0300

    feat: create pixel and pixel matrix

--- rt-proj-01/film.cpp
+++ rt-proj-01/film.cpp
@@ -2,3 +2,7 @@ struct Pixel {
     std::tuple<int, int, int> color; // RGB color tuple
     float depth; // Depth of the pixel
 };
+
+
+using Coordinate = std::pair<int, int>;
+using PixelMatrix = std::map<Coordinate, Pixel>;
